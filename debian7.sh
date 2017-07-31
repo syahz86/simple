ea39deb2e624450d5c9cfded8c26f2d61e3bd950
Tony Esterdark <messi.klate@gmail.com>
2017-08-01 07:35:57 +0800

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -192,10 +192,7 @@ iptables-save
 
 # Restart Service
 chown -R www-data:www-data /home/vps/public_html
-service nginx start
-service php-fpm start
 service vnstat restart
-service openvpn restart
 service ssh restart
 service dropbear restart
 service fail2ban restart
