1427f588c863d7b714e84d0b875f9988dd63b7f5
Tony Esterdark <messi.klate@gmail.com>
2017-08-03 11:52:36 +0800

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -85,7 +85,7 @@ service fail2ban restart
 apt-get -y install squid3
 wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/syahz86/simple/master/conf/squid.conf"
 MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
-sed -i 's/xxxxxxxxx/$MYIP/g' /etc/squid3/squid.conf;
+sed -i s/xxxxxxxxx/$MYIP/g /etc/squid3/squid.conf;
 service squid3 restart
 
 # install webmin
